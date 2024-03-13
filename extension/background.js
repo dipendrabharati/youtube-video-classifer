6a56ee771130b234e882fdc6ffe359b2cbfae2ca
dipendrabharati <dibsbharati@gmail.com>
2024-03-12 20:25:18 -0700

    works with setTimeout and for active window. It doesn't if the new tab is not active

--- extension/background.js
+++ extension/background.js
@@ -4,19 +4,38 @@ chrome.tabs.onCreated.addListener(function(tab) {
     setTimeout(function() {
     console.log("Waited for 5 seconds");
 
-    currentUrl = tab.url;
-    console.log(currentUrl, tab.id);
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            if (tabs.length > 0) {
+                var currentTab = tabs[0];
+                var currentUrl = currentTab.url;
+                console.log("Current URL:", currentUrl);
 
-    if (currentUrl.includes("www.youtube.com/watch")) {
-        console.log("Inside");
-    }
-
-    chrome.tabs.update(tab.id, { url: "popup.html" });
+                if (currentUrl.includes("www.youtube.com/watch")){
+                    chrome.tabs.update(currentTab.id, { url: "popup.html?newurl=" + currentUrl });
+                }else{
+                    console.log("Current url doesn't contain youtube watch")
+                }
+            }
+        });
 
     console.log("End");
     }, 5000);
 
 });
+    // setTimeout(function() {
+    // console.log("Waited for 5 seconds");
+
+    // currentUrl = tab.url;
+    // console.log(currentUrl, tab.id);
+
+    // if (currentUrl.includes("www.youtube.com/watch")) {
+    //     console.log("Inside");
+    // }
+
+    // chrome.tabs.update(tab.id, { url: "popup.html" });
+
+    // console.log("End");
+    // }, 5000);
     // I want to check if the url of this tab got updated
 
     // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
